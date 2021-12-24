16b67a501fdf8bff7ddcc2aec94bf1ff64116174
Ronald Lamoreaux <gitkey@chindraba.work>
2021-12-24 16:08:57 +0000

    Day 17 Part 2
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

--- Solutions/day_17.pl
+++ Solutions/day_17.pl
@@ -38,12 +38,26 @@ use strict;
 use warnings;
 use Elves::GetData qw( :all );
 use Elves::Reports qw( :all );
+use List::Util qw(sum);
 
 my $VERSION = '0.21.17';
 
 my $result = 0;
 
 my ($x_lo, $x_hi, $y_lo, $y_hi) = slurp_data($main::puzzle_data_file) =~ /-?\d+/g;
+
+sub launch #($x_vel, $y_vel, $x_lo, $x_hi, $y_lo, $y_hi)
+{
+  my ($x_vel, $y_vel, $x_lo, $x_hi, $y_lo, $y_hi) = @_;
+  my ($x, $y) = (0, 0);
+  while ($x <= $x_hi && $y >= $y_lo)
+  {
+    return 1 if $x >= $x_lo && $y <= $y_hi;
+    $x  += $x_vel;
+    $x_vel += 0 <=> $x_vel;
+    $y  += $y_vel--;
+  }
+}
 
 # Part 1
 
@@ -54,6 +68,9 @@ report_number(1, $result);
 die 'Not running part 2 yet.' unless $main::do_part_2;
 # Part 2
 
+$result = sum map {
+    my $x_vel = $_; scalar grep { launch($x_vel, $_, $x_lo, $x_hi, $y_lo, $y_hi) } $y_lo..-$y_lo
+} 1..$x_hi;
 report_number(2, $result);
 
 
